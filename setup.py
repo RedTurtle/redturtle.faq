cabe38dcc8eb1722cba1b1db43aecb2247a07067
Andrea Cecchi <andrea.cecchi85@gmail.com>
2022-01-17 15:00:02 +0100

    Preparing release 1.0.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="redturtle.faq",
-    version="1.0.1.dev0",
+    version="1.0.1",
     description="Faqs management",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
