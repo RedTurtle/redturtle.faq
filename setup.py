8e8052d23ccd8185ba699ff1030e8211e58f82d9
Andrea Cecchi <andrea.cecchi85@gmail.com>
2024-04-22 15:21:09 +0200

    Back to development: 1.0.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="redturtle.faq",
-    version="1.0.2",
+    version="1.0.3.dev0",
     description="Faqs management",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
